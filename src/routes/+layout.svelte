a81d1ea1e20bec2d45c7a289087550eb4df975d5
Attila <attila.ocsag@gmail.com>
2024-11-26 22:04:28 +0100

    meta viewport modified

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 </script>
 
 
-<div class="grid w-screen h-screen grid-rows-[10%,90%] bg-slate-300">
+<div class="grid w-screen h-screen grid-rows-[auto, 1fr] bg-slate-300">
 	<nav class="flex items-center gap-4 p-2 m-0 text-xl font-bold bg-lime-400">
 		<a href="{base}/">Home</a>
 		<a href="{base}/map">Map</a>
