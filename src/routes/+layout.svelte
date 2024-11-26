2c8771b63eb1ce8b58f3a290033c47fe3f5c5764
Attila <attila.ocsag@gmail.com>
2024-11-26 13:05:03 +0100

    modification in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
-	import Navbar from '$lib/Navbar.svelte';
+	import { base } from '$app/paths';
 
   if ("virtualKeyboard" in navigator) {
     navigator.virtualKeyboard.overlaysContent = true;
@@ -10,7 +10,10 @@
 </script>
 
 
-<div class="grid w-screen min-h-screen grid-rows-[auto_1fr] bg-slate-300">
-	<Navbar/>
+<div class="grid w-screen h-screen grid-rows-[10%,90%] bg-slate-300">
+	<nav class="flex items-center gap-4 p-2 m-0 text-xl font-bold bg-lime-400">
+		<a href="{base}/">Home</a>
+		<a href="{base}/map">Map</a>
+	</nav>
 	{@render children()}
 </div>
