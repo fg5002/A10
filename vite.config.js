346b1a675be832c17ad15308885cd4ad97965484
Attila <attila.ocsag@gmail.com>
2024-11-27 11:02:39 +0100

    mkcert re-added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'; 
-import { defineConfig } from 'vite'; 
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({ 
+  server: {
+    https: true,
+    proxy: {},
+  },
   plugins: [ 
-    sveltekit(), 
+    sveltekit(),
+    mkcert(),
     { 
       name: 'configure-response-headers', 
       configureServer: (server) => { 
